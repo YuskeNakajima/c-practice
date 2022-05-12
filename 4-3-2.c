5eb3ea3d96ff49636bd6e2c90f9b606ae76aeb06
yusuke_nakajima <www.yyworld@gmail.com>
2022-05-12 09:30:35 +0900

    苦しんで覚えるC言語 4.3.2 実数（浮動小数点数）の計算

---
+++ 4-3-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// %f指定子 => 実数
+int main(void) {
+    printf("%f\n", 10.0 + 3.0);
+    printf("%f\n", 10.0 - 3.0);
+    printf("%f\n", 10.0 * 3.0);
+    printf("%f\n", 10.0 / 3.0);
+    return 0;
+}
